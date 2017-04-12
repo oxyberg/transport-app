bb238f23b49f39c140c04709fc8ff809c3a8c0df
Matthew <matvey@pravosudov.com>
2017-04-13 00:51:01 +0700

    Change map format

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ $route = new Route();
 $route->loadRoutes('map.json');
 
 echo '<pre>';
-print_r($route->convertVertices($route->adj, 'num'));
+print_r($route->buildRoute('b', 'g'));
 echo '</pre>';
--- lib/Route.php
+++ lib/Route.php
@@ -17,27 +17,32 @@ class Route
 
     public function buildRoute($a, $b)
     {
-
+        $routes = $this->convertVertices($this->adj, 'num'); // convert all vertices to numbers
+        //define('INF', 1000000000); // infinity
+        $s = ord($a) - 97; // starting vertex
+        $n = 10; // change to dynamic
     }
 
     public function convertVertices($array, $to)
     {
         $new = [];
-        $count = 0;
-        foreach ($array as $item)
+        foreach ($array as $vertex => $pairs)
         {
-            if ($to == 'num')
+            $count = 0;
+            foreach ($pairs as $pair)
             {
-                $new[$count][0] = ord($item[0]) - 97;
-                $new[$count][1] = ord($item[1]) - 97;
+                if ($to == 'num')
+                {
+                    $new[ord($vertex) - 97][$count][0] = ord($pair[0]) - 97;
+                    $new[ord($vertex) - 97][$count][1] = $pair[1];
+                }
+                else if ($to == 'sym')
+                {
+                    $new[chr($vertex + 97)][$count][0] = chr($pair[0] + 97);
+                    $new[chr($vertex + 97)][$count][1] = $pair[1];
+                }
+                $count++;
             }
-            else if ($to == 'sym')
-            {
-                $new[$count][0] = chr($item[0] + 97);
-                $new[$count][1] = chr($item[1] + 97);
-            }
-            $new[$count][2] = $item[2];
-            $count++;
         }
         return $new;
     }
