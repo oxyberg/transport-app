223a1073c8bd4c40b59ab19fbaba87b42739e55f
Matthew <matvey@pravosudov.com>
2017-04-13 12:40:00 +0700

    Add Dijkstra route build

--- lib/Route.php
+++ lib/Route.php
@@ -17,10 +17,41 @@ class Route
 
     public function buildRoute($a, $b)
     {
+        define('INFINITY', 1000000000);
         $routes = $this->convertVertices($this->adj, 'num'); // convert all vertices to numbers
-        //define('INF', 1000000000); // infinity
         $s = ord($a) - 97; // starting vertex
-        $n = 10; // change to dynamic
+        $n = count($routes); // number of vertices
+
+        // some Dijkstra magic
+        $d = [];
+        for ($i = 0; $i < $n; $i++) $d[$i] = INFINITY;
+        $d[$s] = 0;
+        $p = [];
+        $u = array_fill(0, $n, 0);
+        for ($i = 0; $i < $n; $i++)
+        {
+            $v = -1;
+            for ($j = 0; $j < $n; $j++) if (! $u[$j] && ($v == -1 || $d[$j] < $d[$v])) $v = $j;
+            if ($d[$v] == INFINITY) break;
+            $u[$v] = true;
+            for ($j = 0; $j < count($routes[$v]); $j++)
+            {
+                $to = $routes[$v][$j][0];
+                $len = $routes[$v][$j][1];
+                if ($d[$v] + $len < $d[$to])
+                {
+                    $d[$to] = $d[$v] + $len;
+                    $p[$to] = $v;
+                }
+            }
+        }
+
+        // trace path
+        $path = [];
+        for ($v = ord($b) - 97; $v != $s; $v = $p[$v]) $path[] = $v;
+        $path[] = $s;
+        $path = array_reverse($path);
+        return $path;
     }
 
     public function convertVertices($array, $to)
