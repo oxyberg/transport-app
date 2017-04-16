7dc5ebc3c3179266efd695db815cd9dfb416d4cf
Matthew <matvey@pravosudov.com>
2017-04-17 00:38:58 +0700

    Add some pretty styles

--- views/result.php
+++ views/result.php
@@ -5,52 +5,58 @@
     <link rel="stylesheet" href="views/style.css">
 </head>
 <body>
-    <h1>Your route's params</h1>
-    <form method="post" action="/">
-        <p>
-            From
-            <select name="x">
-                <?php foreach ($cities as $city => $pairs): ?>
-                <option value="<? echo $city; ?>" <? if ($data['x'] == $city) echo ' selected'; ?>><? echo $city; ?></option>
-                <?php endforeach; ?>
-            </select>
-            to
-            <select name="y">
-                <? foreach ($cities as $city => $pairs): ?>
-                <option value="<? echo $city; ?>" <? if ($data['y'] == $city) echo ' selected'; ?>><? echo $city; ?></option>
-                <? endforeach; ?>
-            </select>
-        </p>
-        <p>
-            Find <input type="radio" name="type" value="cheap" id="cheap" <? if ($data['type'] == 'cheap') echo ' checked'; ?>> <label for="cheap">the cheapest</label><br>
-            or <input type="radio" name="type" id="comf" value="comfortable" <? if ($data['type'] == 'comfortable') echo ' checked'; ?>> <label for="comf">the most comfortable</label>
-        </p>
-        <p><input type="submit" value="Retrace route"></p>
-    </form>
-    <table>
-        <tr class="head">
-            <td>Cities</td>
-            <td>Vehicle</td>
-            <td>Distance</td>
-            <td>Type</td>
-            <td class="sum">Ticket Price</td>
-        </tr>
-        <? foreach ($trip->getListOfTransport() as $vehicle): ?>
-        <tr class="data">
-            <td><? echo $vehicle->getPath()[0]; ?> &rarr; <? echo $vehicle->getPath()[1]; ?></td>
-            <td><? echo get_class($vehicle); ?></td>
-            <td><? echo $vehicle->getDistance(); ?></td>
-            <td><? if ($vehicle->isInternational()) echo 'International'; else echo 'Inner'; ?></td>
-            <td class="sum"><? echo $vehicle->payForTicket(); ?></td>
-        </tr>
-        <? endforeach; ?>
-        <tr class="total">
-            <td>Total</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td class="sum"><b><? echo $trip->calcTotal(); ?></b></td>
-        </tr>
-    </table>
+    <div>
+        <h1>Your route's params</h1>
+        <form method="post" action="/">
+            <p>
+                From
+                <select name="x">
+                    <?php foreach ($cities as $city => $pairs): ?>
+                    <option value="<? echo $city; ?>" <? if ($data['x'] == $city) echo ' selected'; ?>><? echo $city; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                to
+                <select name="y">
+                    <? foreach ($cities as $city => $pairs): ?>
+                    <option value="<? echo $city; ?>" <? if ($data['y'] == $city) echo ' selected'; ?>><? echo $city; ?></option>
+                    <? endforeach; ?>
+                </select>
+            </p>
+            <p>
+                <input type="radio" name="type" value="cheap" id="cheap" <? if ($data['type'] == 'cheap') echo ' checked'; ?>> <label for="cheap">The cheapest</label><br>
+                <input type="radio" name="type" id="comf" value="comfortable" <? if ($data['type'] == 'comfortable') echo ' checked'; ?>> <label for="comf">or the most comfortable way</label>
+            </p>
+            <p><input type="submit" value="Retrace route"></p>
+        </form>
+        <? if ($data['x'] == $data['y']): ?>
+        <h3>You are already here</h3>
+        <? else: ?>
+        <table>
+            <tr class="head">
+                <td>Cities</td>
+                <td>Vehicle</td>
+                <td>Distance</td>
+                <td>Type</td>
+                <td class="sum">Ticket Price</td>
+            </tr>
+            <? foreach ($trip->getListOfTransport() as $vehicle): ?>
+            <tr class="data">
+                <td><? echo $vehicle->getPath()[0]; ?> &rarr; <? echo $vehicle->getPath()[1]; ?></td>
+                <td><? echo get_class($vehicle); ?></td>
+                <td><? echo $vehicle->getDistance(); ?></td>
+                <td><? if ($vehicle->isInternational()) echo 'International'; else echo 'Inner'; ?></td>
+                <td class="sum"><? echo $vehicle->payForTicket(); ?></td>
+            </tr>
+            <? endforeach; ?>
+            <tr class="total">
+                <td>Total</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="sum"><b><? echo $trip->calcTotal(); ?></b></td>
+            </tr>
+        </table>
+        <? endif; ?>
+    </div>
 </body>
 </html>
--- views/welcome.php
+++ views/welcome.php
@@ -5,27 +5,29 @@
     <link rel="stylesheet" href="views/style.css">
 </head>
 <body>
-    <h1>Build your route</h1>
-    <form method="post" action="">
-        <p>
-            From
-            <select name="x">
-                <?php foreach ($cities as $city => $pairs): ?>
-                <option value="<? echo $city; ?>"><? echo $city; ?></option>
-                <?php endforeach; ?>
-            </select>
-            to
-            <select name="y">
-                <?php foreach ($cities as $city => $pairs): ?>
-                <option value="<? echo $city; ?>"><? echo $city; ?></option>
-                <?php endforeach; ?>
-            </select>
-        </p>
-        <p>
-            Find the <input type="radio" name="type" value="cheap" id="cheap" checked> <label for="cheap">cheapest</label><br>
-            or <input type="radio" name="type" id="comf" value="comfortable"> <label for="comf">the most comfortable</label>
-        </p>
-        <p><input type="submit" value="Traceroute"></p>
-    </form>
+    <div>
+        <h1>Build your route</h1>
+        <form method="post" action="">
+            <p>
+                From
+                <select name="x">
+                    <?php foreach ($cities as $city => $pairs): ?>
+                    <option value="<? echo $city; ?>"><? echo $city; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                to
+                <select name="y">
+                    <?php foreach ($cities as $city => $pairs): ?>
+                    <option value="<? echo $city; ?>"><? echo $city; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </p>
+            <p>
+                <input type="radio" name="type" value="cheap" id="cheap" checked> <label for="cheap">The cheapest</label><br>
+                <input type="radio" name="type" id="comf" value="comfortable"> <label for="comf">or the most comfortable way</label>
+            </p>
+            <p><input type="submit" value="Traceroute"></p>
+        </form>
+    </div>
 </body>
 </html>
